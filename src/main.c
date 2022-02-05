c01f57b169d171dfb8d3804e216e12ec16827055
Tibo Stroo <tibo.stroo@hotmail.be>
2022-02-05 19:02:56 +0100

    minimax works (I hope)

--- src/main.c
+++ src/main.c
@@ -6,6 +6,10 @@
 #define AI 'O'
 #define PLAYER 'X'
 
+#define PLAYER_WINS 10
+#define AI_WINS -10
+#define TIE 0
+
 /*
  ______                ______                     ______                                        ______  ______
 /\__  _\__            /\__  _\                   /\__  _\                                      /\  _  \/\__  _\
@@ -22,47 +26,15 @@ typedef struct {
     int y;
 } coordinate;
 
-int minimax(char** board) {
-    return 1;
+int max(int num1, int num2);
+int min(int num1, int num2);
+
+int max(int num1, int num2) {
+    return (num1 > num2) ? num1 : num2;
 }
 
-coordinate best_move(char** board, int size) {
-    int best_score = INT_MIN;
-    coordinate optimal_move;
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            if (!board[i][j]) {
-                board[i][j] = AI;
-                int score = minimax(board);
-                board[i][j] = 0;  // reset
-                best_score = score > best_score ? score : best_score;
-                optimal_move.x = i;
-                optimal_move.y = j;
-            }
-        }
-    }
-    board[optimal_move.x][optimal_move.y] = AI;
-    return optimal_move;
-}
-
-void print_board(char** board, int size) {
-    printf("  ");
-    for (int i = 0; i < size; i++) {
-        printf(" %d", i);
-    }
-    printf("\n");
-
-    for (int i = 0; i < size; i++) {
-        printf("%d |", i);
-        for (int j = 0; j < size; j++) {
-            if (!board[i][j]) {
-                printf(".|");
-            } else {
-                printf("%c|", board[i][j]);
-            }
-        }
-        printf("\n");
-    }
+int min(int num1, int num2) {
+    return (num1 > num2) ? num2 : num1;
 }
 
 int won(char** board, int size, char player, coordinate last_played) {
@@ -107,6 +79,114 @@ int won(char** board, int size, char player, coordinate last_played) {
     return 0;
 }
 
+int check_winner(char** board, int size) {
+    for (int i = 0; i < size; i++) {
+        if (won(board, size, PLAYER, (coordinate){i, 0}) || won(board, size, PLAYER, (coordinate){0, i})) {
+            return PLAYER_WINS;
+        }
+        if (won(board, size, AI, (coordinate){i, 0}) || won(board, size, AI, (coordinate){0, i})) {
+            return AI_WINS;
+        }
+    }
+
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if (!board[i][j]) {
+                return -1;
+            }
+        }
+    }
+
+    return TIE;
+}
+
+int minimax(char** board, int size, int is_maximizing, int alpha, int beta) {
+    // printf("%d\n", rand());
+
+    int winner = check_winner(board, size);
+    if (winner != -1) {
+        return winner;
+    }
+
+    if (is_maximizing) {
+        int best_score = INT_MIN;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (!board[i][j]) {
+                    board[i][j] = AI;
+                    int score = minimax(board, size, 0, alpha, beta);
+                    board[i][j] = 0;
+                    best_score = max(best_score, score);
+                    alpha = max(best_score, alpha);
+                    if (beta <= alpha) {
+                        return best_score;
+                    }
+                }
+            }
+        }
+        return best_score;
+
+    } else {
+        int best_score = INT_MAX;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (!board[i][j]) {
+                    board[i][j] = PLAYER;
+                    int score = minimax(board, size, 1, alpha, beta);
+                    board[i][j] = 0;
+                    best_score = min(best_score, score);
+                    beta = min(beta, best_score);
+                    if (beta <= alpha) {
+                        return best_score;
+                    }
+                }
+            }
+        }
+        return best_score;
+    }
+}
+
+coordinate best_move(char** board, int size) {
+    int best_score = INT_MAX;
+    coordinate optimal_move;
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if (!board[i][j]) {
+                board[i][j] = AI;
+                int score = minimax(board, size, 0, INT_MIN, INT_MAX);
+                board[i][j] = 0;  // reset
+                if (score < best_score) {
+                    best_score = score;
+                    optimal_move.x = i;
+                    optimal_move.y = j;
+                }
+            }
+        }
+    }
+    board[optimal_move.x][optimal_move.y] = AI;
+    return optimal_move;
+}
+
+void print_board(char** board, int size) {
+    printf("  ");
+    for (int i = 0; i < size; i++) {
+        printf(" %d", i);
+    }
+    printf("\n");
+
+    for (int i = 0; i < size; i++) {
+        printf("%d |", i);
+        for (int j = 0; j < size; j++) {
+            if (!board[i][j]) {
+                printf(".|");
+            } else {
+                printf("%c|", board[i][j]);
+            }
+        }
+        printf("\n");
+    }
+}
+
 int main(void) {
     printf("You are playing Tic Tac Toe.\n");
 
@@ -118,7 +198,7 @@ int main(void) {
         printf("> Enter a value (integer) to initialize the size of the grid: ");
         scanf("%d", &size);
 
-        while (!(size >= 3 && size < 100)) {
+        while (!(size >= size && size < 100)) {
             printf("Please enter a valid size.\n");
             printf("> Enter a value (integer) to initialize the size of the grid: ");
             while (getchar() != '\n')
